38f599e7764a1884ace76b47266a5165b118ff70
tariktarik <tarik.bajric@gmail.com>
2021-03-17 10:55:35 +0100

    day09

---
+++ ex11/src/index.js
@@ -0,0 +1,26 @@
+function switchCase(letter) {
+    // Write code below this line
+    
+    var animals;
+    switch (letter) {
+        case "a":
+           animals = "antelope";
+            break;
+        case "b":
+           animals = "bird";
+            break;
+        case "c":
+            animals = "cat";
+            break;
+        default:
+            animals = "stuff";
+            break;
+    }
+    // Write code above this line
+    return animals;
+}
+
+
+console.log(switchCase("a"));
+module.exports = switchCase;
+
